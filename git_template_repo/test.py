e73cc46457220a00dc4e2a2dbdb130b14c201792
Timi Ornik <timi@idi-systems.com>
2021-11-09 15:34:34 +0100

    Add call test

---
+++ git_template_repo/test.py
@@ -0,0 +1,14 @@
+import unittest
+
+import git_template_repo
+
+
+class UnitTests(unittest.TestCase):
+
+    def test_call(self):
+        output = git_template_repo.make_call("git", "--version")
+        self.assertIn(b"git version ", output)
+
+
+if __name__ == "__main__":
+    unittest.main()
